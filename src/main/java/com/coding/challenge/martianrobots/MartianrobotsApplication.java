c845728195f1ff05edca3ec8ca3bc500f8092986
Vivek.Ghai <vivek.ghai@metrobank.plc.uk>
2021-10-17 17:02:05 +0100

    Initial Commit

---
+++ src/main/java/com/coding/challenge/martianrobots/MartianrobotsApplication.java
@@ -0,0 +1,13 @@
+package com.coding.challenge.martianrobots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartianrobotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartianrobotsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coding/challenge/martianrobots/MartianrobotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.challenge.martianrobots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartianrobotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
