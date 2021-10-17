431fd85a119a02cc5e6901719f7ce8bd95192ff5
Vivek.Ghai <vivek.ghai@metrobank.plc.uk>
2021-10-17 17:31:55 +0100

    Adding enums for allowed Orientation and Robot Instruction values

---
+++ src/main/java/com/coding/challenge/martianrobots/RobotInstruction.java
@@ -0,0 +1,9 @@
+package com.coding.challenge.martianrobots;
+
+//AN ENUMERATION TO ENSURE FIXED SET OF INSTRUCTIONS ARE PASSED IN THE INPUT
+public enum RobotInstruction {
+
+    L,
+    R,
+    F
+}
